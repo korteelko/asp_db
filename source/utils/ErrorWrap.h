e56f41e4c6391cdbdbbc67d33ed374730fa38415
jiri <korteelko@gmail.com>
2020-07-09 00:27:14 +0300

    First commit

---
+++ source/utils/ErrorWrap.h
@@ -0,0 +1,109 @@
+/**
+ * asp_therm - implementation of real gas equations of state
+ *
+ *
+ * Copyright (c) 2020 Mishutinski Yurii
+ *
+ * This library is distributed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+#ifndef UTILS__ERRORWRAP_H
+#define UTILS__ERRORWRAP_H
+
+#include "Common.h"
+#include "ThreadWrap.h"
+/** \note Для разных проектов разные коды ошибок,
+  *   поэтому дефайны вынесены в отдельный файл */
+#if defined(INCLUDE_ERRORCODES)
+#  include "merror_codes.h"
+#endif  // INCLUDE_ERRORCODES
+
+#include <string>
+
+#include <stdint.h>
+
+
+#if defined(_DEBUG)
+#  define STRING_DEBUG_INFO ("file: " + std::string(__FILE__) + \
+       "\n\tfunction: " + std::string(__FUNCTION__) \
+       + " line: " + std::to_string(__LINE__) + "\n")
+#else
+#  define STRING_DEBUG_INFO ""
+#endif  // _DEBUG
+
+typedef uint32_t merror_t;
+/** \brief ошибка файлового ввода/вывода */
+#define ERROR_FILEIO_T        0x0002
+/** \brief ошибка при работе со строками */
+#define ERROR_STRING_T        0x0004
+/** \brief ошибка инициализации */
+#define ERROR_INIT_T          0x0005
+//   fileio errors
+/** \brief ошибка чтения файла */
+#define ERROR_FILE_IN_ST      (0x0100 | ERROR_FILEIO_T)
+/** \brief ошибка записи в файл */
+#define ERROR_FILE_OUT_ST     (0x0200 | ERROR_FILEIO_T)
+/** \brief ошибка операции с файлом логирования */
+#define ERROR_FILE_LOGGING_ST (0x0300 | ERROR_FILEIO_T)
+/** \brief ошибка существования файла */
+#define ERROR_FILE_EXISTS_ST  (0x0400 | ERROR_FILEIO_T)
+
+//   string errors
+#define ERROR_STR_MAX_LEN_ST  (0x0100 | ERROR_STRING_T)
+#define ERROR_STR_PARSE_ST    (0x0200 | ERROR_STRING_T)
+#define ERROR_STR_NULL_ST     (0x0300 | ERROR_STRING_T)
+#define ERROR_STR_TOINT_ST    (0x0400 | ERROR_STRING_T)
+
+//   init errors
+#define ERROR_INIT_ZERO_ST    (0x0100 | ERROR_INIT_T)
+#define ERROR_INIT_NULLP_ST   (0x0200 | ERROR_INIT_T)
+
+/** \brief класс, в котором инкапсулирована ошибка(код, сообщение,
+  *   логирована ли, выведелена ли и т.п.) */
+class ErrorWrap {
+public:
+  ErrorWrap();
+  explicit ErrorWrap(merror_t error);
+  ErrorWrap(merror_t error, const std::string &msg);
+  /** \brief установить(хранить) код ошибки 'error'
+    * \param error код ошибки
+    * \param(optional) msg сопроводительное сообщение */
+  merror_t SetError(merror_t error);
+  merror_t SetError(merror_t error, const std::string &msg);
+  /** \brief заменить сообщение об ошибке 'msg_' на 'msg' */
+  void SetErrorMessage(const std::string &msg);
+  /** \brief залогировать текущее состояние(если есть ошибка)
+    *   установить 'is_logged_' в true
+    * \param lvl(optional) особый логлевел для данного сообщения
+    * \note здесь и везде Log* методы не константные,
+    *   т.к. в них отслеживается состояние */
+  void LogIt();
+  void LogIt(io_loglvl lvl);
+
+  merror_t GetErrorCode() const;
+  std::string GetMessage() const;
+  bool IsLogged() const;
+
+  /** \brief скинуть все параметры объекта ErrorWrap в значения по умолчанию:
+    *   error_ to ERROR_SUCCESS_T
+    *   msg_ to ""
+    *   is_logged_ to false */
+  void Reset();
+
+  /* DEVELOP: такая перегрузка может стать причиной ментального бо-бо */
+  ErrorWrap &operator= (merror_t) = delete;
+
+private:
+  /** \brief код ошибки(см merror_codes.h) */
+  merror_t error_;
+  /** \brief сообщение к ошибке */
+  std::string msg_;
+  /** \brief мьютекс на обновление данных, на логирование */
+  Mutex update_mutex_;
+  /** \brief переменная отслеживающая выводилось ли уже
+    *   информация об этой ошибке
+    * \note скидываем на false при обновлении ошибки(SetError) */
+  bool is_logged_;
+};
+
+#endif  // !UTILS__ERRORWRAP_H
---
+++ source/utils/FileURL.h
@@ -0,0 +1,125 @@
+/**
+ * asp_therm - implementation of real gas equations of state
+ * ===================================================================
+ * * FileURL *
+ *   В файле описан функционал оборачивающий адрессацию данных
+ * из внешних источников
+ * ===================================================================
+ *
+ * Copyright (c) 2020 Mishutinski Yurii
+ *
+ * This library is distributed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+#ifndef UTILS__FILEURL_H
+#define UTILS__FILEURL_H
+
+#include "Common.h"
+#include "ErrorWrap.h"
+
+#include <string>
+
+
+namespace file_utils {
+/* todo: create class file_utils exception */
+class FileURLRoot;
+
+/** \brief тип задания адреса файла, буффера памяти и т.п. */
+enum class url_t {
+  empty = 0,
+  /** \brief путь в файловой системе */
+  fs_path = 1
+};
+
+/* todo: replace 'root' type with struct wth overloaded operator[]
+ *   for c++17 comp */
+/** \brief сетап URL - хост, юзер и т.п., все входные
+  *   данные для фабрики URL объектов короч
+  * \note сейчас то это просто для красоты */
+struct SetupURL {
+public:
+  SetupURL(url_t url_type, const std::string &root);
+  /** \brief Полный префикс для пути(от начала до рута) */
+  std::string GetFullPrefix();
+  /** \brief Тип ссылки */
+  url_t GetURLType();
+
+public:
+  /** \brief тип адресов для создания полных путей */
+  url_t url_type;
+  /** \brief путь к корню системы(для файловой системы) */
+  std::string root;
+};
+inline std::string SetupURL::GetFullPrefix() { return root; }
+inline url_t SetupURL::GetURLType() { return url_type; }
+
+/** \brief класс пути(м.б. обычный пути в файловой системе, урл,
+  *   запрос к дб и т.п.)
+  * \note немножко ликбеза:
+  *   вид File(URI):
+  *     "file://<host>/<path>"
+  *   А вот обощённый URL несёт много больше информации:
+  *     "<схема>:[//[<логин>[:<пароль>]@]<хост>[:<порт>]][/<URL‐путь>][?<параметры>][#<якорь>]"
+  *   Естественно, полный url нам пока не нужен, но, лучше ввести лишнюю
+  *     структуру хранящую все параметры подключения(хост, юзер с поролем),
+  *     которую будем хранить в фабрике */
+class FileURL {
+  friend class FileURLRoot;
+
+public:
+  /** \brief Полный путь к файлу, области памяти */
+  std::string GetURL() const;
+  /** \brief Получить код ошибки */
+  merror_t GetError() const;
+
+  /** \brief Путь отмечен как невалидный */
+  bool IsInvalidPath() const;
+  /** \brief Установить код ошибки 'error' */
+  void SetError(merror_t error);
+  /** \brief Установить код ошибки 'error' и сообщение ошибки 'msg' */
+  void SetError(merror_t error, const std::string &msg);
+  /** \brief Залогировать ошибки */
+  void LogError();
+
+private:
+  FileURL();
+  FileURL(url_t url_type, const std::string &path);
+
+private:
+  ErrorWrap error_;
+  mstatus_t status_;
+  /** \brief тип адреса */
+  url_t url_type_;
+  /** \brief полный путь к файлу */
+  std::string absolute_path_;
+};
+inline std::string FileURL::GetURL() const { return absolute_path_; }
+inline merror_t FileURL::GetError() const { return error_.GetErrorCode(); }
+
+
+/** \brief Фабрика инициализации файловых адресов */
+class FileURLRoot {
+public:
+  FileURLRoot(const SetupURL &setup);
+  FileURLRoot(url_t url_type, const std::string &root);
+
+  /** \brief Инициализация прошла успешно */
+  bool IsInitialized();
+  /** \brief Собрать адрес файла по относительному пути */
+  FileURL CreateFileURL(const std::string &relative_path);
+
+private:
+  /** \brief Проверить root директорию файловой системы */
+  void check_fs_root();
+  /** \brief Собрать адрес файла для случая файловой системы */
+  FileURL set_fs_path(const std::string &relative_path);
+  /** \brief Проверить что путь относительный, а не абсолютный */
+  bool is_absolute_path(const std::string &path);
+
+private:
+  mstatus_t status_;
+  SetupURL setup_;
+};
+}  // namespace file_unils
+
+#endif  // !UTILS__FILEURL_H
