779f5494b82c2d4a12237f1614b694f28ef670de
jiri <korteelko@gmail.com>
2020-07-10 23:47:19 +0300

    fix cmake

---
+++ db_source.cmake
@@ -0,0 +1,9 @@
+set(DATABASE_SOURCE
+  database/db_connection.cpp
+  database/db_connection_postgre.cpp
+  database/db_connection_manager.cpp
+  database/db_defines.cpp
+  database/db_queries_setup.cpp
+  database/db_query.cpp
+)
+
