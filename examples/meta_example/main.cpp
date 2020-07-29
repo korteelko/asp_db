d47e11526242e816c785d37e8c839ed63358616b
jiri <korteelko@gmail.com>
2020-07-29 23:51:44 +0300

    заготовка под ещё один пример

---
+++ examples/meta_example/main.cpp
@@ -0,0 +1,16 @@
+#include "example_tables.h"
+
+#include <iostream>
+
+#include <assert.h>
+
+
+int main() {
+  example1_info e1 {1, "odin"};
+  example2_info e2 {.id = 2, .name = "dva", .strings = {"x", "yy", "zzz"},
+      .num = 4, .fkey = {0, &e1}};
+  // and
+  std::cout << e1;
+  std::cout << e2;
+  return 0;
+}
