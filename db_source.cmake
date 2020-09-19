2b22c7c70765c406402dd866adcfb9d20791ddfa
jiri <korteelko@gmail.com>
2020-09-19 20:51:52 +0300

    module refactoring

--- db_source.cmake
+++ db_source.cmake
@@ -1,11 +1,11 @@
 # .cmake файл для включения модуля как набора сырцов, а не библиотеки
 set(DATABASE_SOURCE
-  "${ASP_DB_ROOT}/source/database/db_connection.cpp"
-  "${ASP_DB_ROOT}/source/database/db_connection_postgre.cpp"
-  "${ASP_DB_ROOT}/source/database/db_connection_manager.cpp"
-  "${ASP_DB_ROOT}/source/database/db_defines.cpp"
-  "${ASP_DB_ROOT}/source/database/db_queries_setup.cpp"
-  "${ASP_DB_ROOT}/source/database/db_query.cpp"
+  "${ASP_DB_ROOT}/source/db_connection.cpp"
+  "${ASP_DB_ROOT}/source/db_connection_postgre.cpp"
+  "${ASP_DB_ROOT}/source/db_connection_manager.cpp"
+  "${ASP_DB_ROOT}/source/db_defines.cpp"
+  "${ASP_DB_ROOT}/source/db_queries_setup.cpp"
+  "${ASP_DB_ROOT}/source/db_query.cpp"
 )
 
-include_directories(${ASP_DB_ROOT}/source/database)
+include_directories(${ASP_DB_ROOT}/source)
