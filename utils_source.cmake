a7f41b884f74186683b3c4b87d488d5d35fa90b8
jiri <korteelko@gmail.com>
2020-09-20 09:52:54 +0300

    fix cmake files

--- utils_source.cmake
+++
@@ -1,6 +0,0 @@
-set(UTILS_SOURCE
-  "${ASP_DB_ROOT}/source/utils/Common.cpp"
-  "${ASP_DB_ROOT}/source/utils/ErrorWrap.cpp"
-  "${ASP_DB_ROOT}/source/utils/Logging.cpp"
-)
-include_directories(${ASP_DB_ROOT}/source/utils)
